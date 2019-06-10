613f1d4c106f814ecb589218ec30beb452081138
Claudia Solbes <cssolbes@hotmail.com>
2019-06-10 01:44:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else katz_deli
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  end
+end
+
+def take_a_number(katz_deli, number)
+  new_line = ["Ada", "Grace", "Kent"]
+  new_line.each_with_index do |person, number|
+  puts "Welcome, #{person}. You are number #{number} in line."
+  end
+end

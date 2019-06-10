bd0bfd22b5babc67afd087a1f30549fc0d95483f
Claudia Solbes <cssolbes@hotmail.com>
2019-06-10 02:21:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,6 @@ def now_serving(katz_deli)
   if katz_deli == []
    puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{costumer}"
+    puts "Currently serving #{costumer}."
   end
 end
